3433bdebd088273bf19788039b368365bfce6dbf
Kim Hammar <kimham@kth.se>
2020-03-15 16:00:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# `gym-yagw` Yet Another Grid World
+
+## Author & Maintainer
+
+Kim Hammar <kimham@kth.se>
+
+## Copyright and license
+
+[LICENSE](LICENSE.md)
+
+MIT
+
+(C) 2020, Kim Hammar
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='gym_yagw',
+      version='0.0.1',
+      install_requires=['gym', 'pyglet'],
+      author='Kim Hammar',
+      author_email='hammar.kim@gmail.com',
+      description='YAGW - Yet Another Grid World',
+      license='MIT License',
+      keywords='Gridworld, reinforcement learning, Q-learning',
+      url='',
+      download_url='TODO',
+)
+
+
+
